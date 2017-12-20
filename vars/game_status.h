203d79d2e1c4fc22c337b32c2441c062a21cfa9c
marianexyx <mariusz.pak.89@gmail.com>
2017-12-20 23:38:37 +0100

    Dodano komentarz do game_status.h o brakujacej zawartosci

--- vars/game_status.h
+++ vars/game_status.h
@@ -4,6 +4,30 @@
 enum GAME_STATUS
 {
     //GS_
+
+    /*
+     * TURN_NONE
+    WAITING_FOR_PLAYERS
+    WAITING_FOR_START_CONFIRMS
+    GAME_HAS_ENDED:
+        WHIE_WON,
+        BLACK_WON,
+        DRAW,
+        STAND_UP,
+        GIVE_UP,
+        SOCKET_LOST,
+        TIMEOUT_START_3
+        TIMEOUT_WHITE,
+        TIMEOUT_BLACK
+    RESETING
+    RESET_COMPLITED
+TURN_WHITE/BLACK
+    FIRST_TURN
+    MOVE_OK_E2E4
+    BAD_MOVE_E2E4
+    PROMOTE
+    PROMOTED_E7E8Q
+*/
 };
 
 
